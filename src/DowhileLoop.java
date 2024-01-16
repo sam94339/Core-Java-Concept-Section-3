bd3f4fd2e00d4ec29c339b0b955c2617a466da96
Soumyadeep Ghosh <sam94339@gmail.com>
2024-01-16 19:53:10 +0530

    Soumyadeep Ghosh

---
+++ src/DowhileLoop.java
@@ -0,0 +1,17 @@
+
+public class DowhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i=20;
+		
+		do {
+			
+			System.out.println(i);
+			i++;
+			
+		}while(i<30); //In do while, 1 loop of execution is guarantee. In the while loop, 1st need condition to execute but for do while for 1st time no need any condition.
+
+	}
+
+}
